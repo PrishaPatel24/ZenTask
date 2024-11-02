cd8991cd45962b240e9ad7320392b3c1137d8c3e
stickycorner <jennazhang.jz@gmail.com>
2024-11-02 09:13:42 -0400

    updated entities

--- src/main/java/entity/Note.java
+++ src/main/java/entity/Note.java
@@ -1,4 +1,23 @@
 package entity;
 
+/**
+ * The representation of a note file for this program.
+ */
 public class Note {
+
+    private final String content;
+    private final String creationDate;
+
+    public Note(String content, String creationDate) {
+        this.content = content;
+        this.creationDate = creationDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
 }
