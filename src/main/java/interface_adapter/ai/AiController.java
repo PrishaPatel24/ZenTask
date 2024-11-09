792a2d6015b282c604ac7c80270bce051b495978
stickycorner <jennazhang.jz@gmail.com>
2024-11-09 11:11:05 -0500

    updated view package

--- src/main/java/interface_adapter/ai/AiController.java
+++ src/main/java/interface_adapter/ai/AiController.java
@@ -1,4 +1,20 @@
 package interface_adapter.ai;
 
+import use_cases.ai.AiInputBoundary;
+
 public class AiController {
+    private final AiInputBoundary aiInteractor;
+
+    public AiController(AiInputBoundary aiInputBoundary) {
+        this.aiInteractor = aiInputBoundary;
+    }
+
+    /**
+     * Generates the AI response from a given note.
+     * @param note The note in the JTextArea written by the user.
+     */
+    public void generateResponse(String note) {
+        aiInteractor.generateResponse(note + "Please complete the note based on the"
+                + "given notes.");
+    }
 }
---
+++ src/main/java/view/ChecklistView.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+
+/**
+ * The view and taking in the inputs for the checklist section of the program.
+ */
+public class ChecklistView {
+    private final JButton taskButton;
+    private final JLabel label;
+
+    public ChecklistView() {
+        final JPanel checklistPanel = new JPanel();
+        this.label = new JLabel("Tasks go here!");
+        checklistPanel.add(label);
+        this.taskButton = new JButton("Add Task +");
+        checklistPanel.add(taskButton);
+
+        taskButton.addActionListener(
+                evt -> {
+                    if (evt.getSource().equals(taskButton)) {
+                        // add ChecklistController.dosomething() based
+                        // on button
+                    }
+                }
+        );
+    }
+}
